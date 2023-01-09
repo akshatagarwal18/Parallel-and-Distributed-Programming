1a597151133974f27d4bff99b5684d3daadb2a01
Akshat Agarwal <akshat_a@cs.iitr.ac.in>
2023-01-09 12:28:03 +0530

    Initialized matrix to zero.

--- Parallel Matrix Multiplication/Non_Blocking_Call_MatrixMultiplication.c
+++ Parallel Matrix Multiplication/Non_Blocking_Call_MatrixMultiplication.c
@@ -7,7 +7,7 @@
 #define MASTER 0
 #define MASTER_TO_SLAVE_TAG 0
 #define SLAVE_TO_MASTER_TAG 1
-#define THIRTY_TWO 32
+#define THIRTY_TWO 2
 
 int isEqual(float* A , float* B, int row, int col){
     for(int i = 0 ; i < row ; i++){
@@ -32,8 +32,9 @@ void print_matrix(float* A, int row, int col){
     for(int i = 0 ; i < row ; i++){
         for(int j = 0 ; j < col ; j++){
             printf("%6.3f ",A[i*row+j]);
-        }
-    }
+        } 
+        printf("\n");
+    }printf("\n");
 }
 
 float* create_array(int n, int m){
@@ -45,8 +46,8 @@ void print_error(int n , float* A, float* B, float* C){
     for(int i = 0 ; i < n ; i++){
         for(int j = 0 ; j < n ; j++){
              D[i*n + j] = 0.0 ;
-            for(int k = 0 ; k < 32 ; k++){
-                D[i*n + j] = D[i*n+j] + A[i*32 + k]*B[k*n + j];
+            for(int k = 0 ; k < THIRTY_TWO ; k++){
+                D[i*n + j] = D[i*n+j] + A[i*THIRTY_TWO + k]*B[k*n + j];
             }
         }
     } float err = 0.0;
@@ -68,6 +69,7 @@ int main(int argc, char* argv[]){
     int rank; MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     int size; MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Status status;
+    MPI_Request request;
     if(size <= 1){
         MPI_Abort(MPI_COMM_WORLD, 0);
         exit(1);
@@ -79,56 +81,45 @@ int main(int argc, char* argv[]){
         C = create_array(n,n);
         srand((unsigned int)time(NULL));
         for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < 32 ; j++){
-                A[i*32 + j] = rand()/(float)RAND_MAX ;
+            for(int j = 0 ; j < THIRTY_TWO ; j++){
+                A[i*THIRTY_TWO + j] = rand()/(float)RAND_MAX ;
             }
         }
-        for(int i = 0 ; i < 32 ; i++){
+        for(int i = 0 ; i < THIRTY_TWO ; i++){
             for(int j = 0 ; j < n ; j++){
                 B[i*n + j] = rand()/(float)RAND_MAX ;
             }
         }
 
-        row_partition = n/size;
-        extra_rows = n%size;
+        row_partition = n/(size-1);
+        extra_rows = n%(size-1);
         tag = MASTER_TO_SLAVE_TAG;
         total_elems = 0;
         double start = MPI_Wtime();
-        for(int i = 0 ; i < size; i++){
+        for(int i = 1 ; i < size; i++){
             send_rows = row_partition;
-            if(i < extra_rows){send_rows++;}
-            if(i == 0){
-                first = send_rows;
-                total_elems+=send_rows;
-                continue;
-            }
-            MPI_Send(&total_elems,1,MPI_INT,i,tag,MPI_COMM_WORLD);
-            MPI_Send(&send_rows,1,MPI_INT,i,tag,MPI_COMM_WORLD);
-            MPI_Send(A+THIRTY_TWO*total_elems, send_rows*THIRTY_TWO, MPI_FLOAT, i, tag, MPI_COMM_WORLD);
-            MPI_Send(B, THIRTY_TWO*n, MPI_FLOAT, i, tag, MPI_COMM_WORLD);
+            if(i <= extra_rows){send_rows++;}            
+            MPI_Isend(&total_elems,1,MPI_INT,i,tag,MPI_COMM_WORLD,&request);
+            MPI_Isend(&send_rows,1,MPI_INT,i,tag,MPI_COMM_WORLD,&request);
+            MPI_Wait(&request, &status);
+            MPI_Isend(A+THIRTY_TWO*total_elems, send_rows*THIRTY_TWO, MPI_FLOAT, i, tag, MPI_COMM_WORLD, &request);
+            MPI_Isend(B, THIRTY_TWO*n, MPI_FLOAT, i, tag, MPI_COMM_WORLD,&request);
+            MPI_Wait(&request, &status);
             total_elems+=send_rows;
         }
-
-        for(int i = 0 ; i < first ; i++){
-            for(int j = 0 ; j < n ; j++){
-                C[i*n+j]=0.0;
-                for(int k = 0 ; k < 32 ; k++){
-                    C[i*n+j]+=(A[i*32+k]*B[k*n+j]);
-                }
-            }
-        }
-
         tag = SLAVE_TO_MASTER_TAG;
         for(int i = 1 ; i < size ; i++){
-            MPI_Recv(&total_elems,1,MPI_INT,i,tag,MPI_COMM_WORLD,&status);
-            MPI_Recv(&send_rows,1,MPI_INT,i,tag,MPI_COMM_WORLD,&status);
-            MPI_Recv(C+n*total_elems,send_rows*n,MPI_FLOAT,i,tag,MPI_COMM_WORLD,&status);
+            MPI_Irecv(&total_elems,1,MPI_INT,i,tag,MPI_COMM_WORLD,&request);
+            MPI_Irecv(&send_rows,1,MPI_INT,i,tag,MPI_COMM_WORLD,&request);
+            MPI_Wait(&request, &status);
+            MPI_Irecv(C+n*total_elems,send_rows*n,MPI_FLOAT,i,tag,MPI_COMM_WORLD,&request);
+            MPI_Wait(&request, &status);
         }
         double end = MPI_Wtime();
         double tim = end-start;
 
-        print_matrix(A,n,32);
-        print_matrix(B,32,n);
+        print_matrix(A,n,THIRTY_TWO);
+        print_matrix(B,THIRTY_TWO,n);
         print_matrix(C,n,n);
         print_error(n,A,B,C);
         printf("Time taken for matrix multiplication is %0.4fs\n ", tim);
@@ -136,29 +127,33 @@ int main(int argc, char* argv[]){
     }
     else{
         tag = MASTER_TO_SLAVE_TAG;
-        MPI_Recv(&total_elems, 1, MPI_INT, MASTER, tag, MPI_COMM_WORLD, &status);
-        MPI_Recv(&send_rows, 1, MPI_INT, MASTER, tag, MPI_COMM_WORLD, &status);
+        MPI_Irecv(&total_elems, 1, MPI_INT, MASTER, tag, MPI_COMM_WORLD, &request);
+        MPI_Irecv(&send_rows, 1, MPI_INT, MASTER, tag, MPI_COMM_WORLD, &request);
+        MPI_Wait(&request, &status);
 
         A = create_array(send_rows,THIRTY_TWO);
         B = create_array(THIRTY_TWO,n);
         C = create_array(send_rows,n);
 
-        MPI_Recv(A, send_rows*THIRTY_TWO, MPI_FLOAT, MASTER, tag, MPI_COMM_WORLD, &status);
-        MPI_Recv(B, THIRTY_TWO*n, MPI_FLOAT, MASTER, tag, MPI_COMM_WORLD, &status);
+        MPI_Irecv(A, send_rows*THIRTY_TWO, MPI_FLOAT, MASTER, tag, MPI_COMM_WORLD, &request);
+        MPI_Irecv(B, THIRTY_TWO*n, MPI_FLOAT, MASTER, tag, MPI_COMM_WORLD, &request);
+        MPI_Wait(&request, &status);
 
         for(int i = 0 ; i < send_rows ; i++){
             for(int j = 0 ; j < n ; j++){
                 C[i*n+j]=0.0;
-                for(int k = 0 ; k < 32 ; k++){
-                    C[i*n+j]+=(A[i*32+k]*B[k*n+j]);
+                for(int k = 0 ; k < THIRTY_TWO ; k++){
+                    C[i*n+j]+=(A[i*THIRTY_TWO+k]*B[k*n+j]);
                 }
             }
         }
         tag = SLAVE_TO_MASTER_TAG;
 
-        MPI_Send(&total_elems, 1, MPI_INT, MASTER, tag, MPI_COMM_WORLD);
-        MPI_Send(&send_rows, 1, MPI_INT, MASTER, tag, MPI_COMM_WORLD);
-        MPI_Send(C,send_rows*n, MPI_FLOAT, MASTER, tag, MPI_COMM_WORLD);
+        MPI_Isend(&total_elems, 1, MPI_INT, MASTER, tag, MPI_COMM_WORLD,&request);
+        MPI_Isend(&send_rows, 1, MPI_INT, MASTER, tag, MPI_COMM_WORLD,&request);
+        MPI_Wait(&request, &status);
+        MPI_Isend(C,send_rows*n, MPI_FLOAT, MASTER, tag, MPI_COMM_WORLD,&request);
+        MPI_Wait(&request, &status);
     }
     MPI_Finalize();
 }
